7dc105f9cf13ede53a13bba4df2373a6ccbbae55
Choonghee Lee <choonghee.dev@gmail.com>
2020-06-24 08:21:00 +0900

    colors to answers and helps
    volumes and surface area of a cylinder
    color for each answer
    color for each help

--- arithmetics/commands.py
+++ arithmetics/commands.py
@@ -1,15 +1,20 @@
 import click
+from click_help_colors import HelpColorsGroup, HelpColorsCommand
 from math import pi
 
-@click.group()
+@click.group(
+    cls=HelpColorsGroup,
+    help_headers_color="yellow",
+    help_options_color="magenta"
+)
 def arithmetics():
     pass
 
 # 팩토리얼 구하기
-@arithmetics.command()
+@arithmetics.command(cls=HelpColorsCommand, help_options_color="magenta")
 @click.argument('num', type=int)
 def fact(num):
-    """Calculate factorial value."""
+    """Calculate factorial value"""
     if num < 0:
         return 0
     if num == 0:
@@ -18,29 +23,40 @@ def fact(num):
     f = 1
     for i in range(1, num + 1):
         f = f * i
-    click.echo(f)
+    click.secho(f"{f}", fg="blue", bg="white")
 
 
 # 거듭제곱
-@arithmetics.command()
+@arithmetics.command(cls=HelpColorsCommand, help_options_color="magenta")
 @click.argument('base', type=float)
 @click.argument('exponent', type=float)
 def pow(base, exponent):
     """Get the power of a number"""
-    click.echo(base**exponent)
+    click.secho(f"{base**exponent}", fg="blue", bg="white")
 
 
 # 도를 라디안으로 변환
-@arithmetics.command()
+@arithmetics.command(cls=HelpColorsCommand, help_options_color="magenta")
 @click.argument('degree', type=float)
 def dtr(degree):
     """Convert degree to radian"""
-    click.echo(degree*(pi/180))
+    click.secho(f"{degree*(pi/180)}", fg="blue", bg="white")
+    
 
 
 # 라디안을 도로 변환
-@arithmetics.command()
+@arithmetics.command(cls=HelpColorsCommand, help_options_color="magenta")
 @click.argument('radian', type=float)
 def rtd(radian):
     """Convert radian to degree"""
-    click.echo(radian*(180/pi))
+    click.secho(f"{radian*(180/pi)}", fg="blue", bg="white")
+
+
+# 원통의 부피와 넓이
+@arithmetics.command(cls=HelpColorsCommand, help_options_color="magenta")
+@click.argument('height', type=float)
+@click.argument('radian', type=float)
+def vaoc(height, radian):
+    """Calculate surface volume and area of a cylinder"""
+    click.secho(f"Volume: {pi*radian*radian*height}", fg="blue", bg="white")
+    click.secho(f"Area: {((2*pi*radian) * height) + ((pi*radian**2)*2)}", fg="blue", bg="white")
--- main.py
+++ main.py
@@ -1,8 +1,13 @@
 import click
+from click_help_colors import HelpColorsGroup
 from arithmetics.commands import arithmetics
 
-@click.version_option('0.0.1', prog_name="Calculator From CLI")
-@click.group()
+@click.version_option('0.1.0', prog_name="Calculator From CLI")
+@click.group(
+    cls=HelpColorsGroup,
+    help_headers_color="yellow",
+    help_options_color="magenta"
+)
 def main():
     """Calculator From CLI"""
     pass
