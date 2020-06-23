80a74894d93426481d5ce611b89e62ffa02bccf8
Choonghee Lee <choonghee.dev@gmail.com>
2020-06-24 08:34:56 +0900

    factorial method options and app tree
    method option in [1,2,3]
        1. for loop
        2. recursive function
        3. math module function
    
    now you can use tree command

--- arithmetics/commands.py
+++ arithmetics/commands.py
@@ -1,6 +1,6 @@
 import click
 from click_help_colors import HelpColorsGroup, HelpColorsCommand
-from math import pi
+from math import pi, factorial
 
 @click.group(
     cls=HelpColorsGroup,
@@ -10,19 +10,42 @@ from math import pi
 def arithmetics():
     pass
 
-# 팩토리얼 구하기
-@arithmetics.command(cls=HelpColorsCommand, help_options_color="magenta")
-@click.argument('num', type=int)
-def fact(num):
-    """Calculate factorial value"""
-    if num < 0:
+def factorial1(n):
+    if n < 0:
         return 0
-    if num == 0:
+    if n == 0:
         return 1
 
     f = 1
-    for i in range(1, num + 1):
+    for i in range(1, n + 1):
         f = f * i
+    return f
+
+def factorial2(n):
+    if n < 0:
+        return 0
+    if n == 0:
+        return 1
+
+    return n * factorial2(n - 1)
+
+
+# 팩토리얼 구하기
+@arithmetics.command(cls=HelpColorsCommand, help_options_color="magenta")
+@click.option('--method', '-m', type=int, default=1, help="Choose a number of factorial methods")
+@click.argument('num', type=int)
+def fact(method, num):
+    """Calculate factorial value"""
+    f = None
+    if method == 1:
+        f = factorial1(num)
+    elif method == 2:
+        f = factorial2(num)
+    elif method == 3:
+        f = factorial(num)
+    else:
+        click.secho("Wrong method number!", fg="red", bg="yellow")
+        return
     click.secho(f"{f}", fg="blue", bg="white")
 
 
--- main.py
+++ main.py
@@ -1,12 +1,15 @@
 import click
+from click_plugins import with_plugins
+from pkg_resources import iter_entry_points
 from click_help_colors import HelpColorsGroup
 from arithmetics.commands import arithmetics
 
 @click.version_option('0.1.0', prog_name="Calculator From CLI")
+@with_plugins(iter_entry_points("click_command_tree"))
 @click.group(
     cls=HelpColorsGroup,
     help_headers_color="yellow",
-    help_options_color="magenta"
+    help_options_color="magenta",
 )
 def main():
     """Calculator From CLI"""
