fbd6b06d96e13ece8eaf87935937f10656799cbe
Choonghee Lee <choonghee.dev@gmail.com>
2020-06-24 10:23:34 +0900

    test publish

--- bambic/__main__.py
+++ bambic/__main__.py
@@ -1,6 +1,19 @@
 import click
+from click_plugins import with_plugins
+from pkg_resources import iter_entry_points
 from click_help_colors import HelpColorsGroup, HelpColorsCommand
 from math import pi, factorial
+
+@click.version_option('0.1.0', prog_name="Calculator From CLI")
+@with_plugins(iter_entry_points("click_command_tree"))
+@click.group(
+    cls=HelpColorsGroup,
+    help_headers_color="yellow",
+    help_options_color="magenta",
+)
+def main():
+    """Bambi Calculator From CLI"""
+    pass
 
 @click.group(
     cls=HelpColorsGroup,
@@ -83,3 +96,7 @@ def vaoc(height, radian):
     """Calculate surface volume and area of a cylinder"""
     click.secho(f"Volume: {pi*radian*radian*height}", fg="blue", bg="white")
     click.secho(f"Area: {((2*pi*radian) * height) + ((pi*radian**2)*2)}", fg="blue", bg="white")
+
+
+if __name__ == "__main__":
+    main()
